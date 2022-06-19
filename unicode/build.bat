9479454787ae3a8329b0901643032fa89e16b6e1
slaakko <slaakko@gmail.com>
2022-06-19 20:10:12 +0300

    docs

--- unicode/build.bat
+++ unicode/build.bat
@@ -1,3 +1,3 @@
 @echo off
 IF NOT EXIST ucd.all.flat.xml powershell -command "Expand-Archive -Force '%~dp0ucd.all.flat.zip' '%~dp0'"
-IF NOT EXIST soul_ucd.bin make_soul_ucd
+IF NOT EXIST soul_ucd.bin ..\bin\make_soul_ucd
